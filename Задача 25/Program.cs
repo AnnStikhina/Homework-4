ac42cfa7ca831955b11f868e80214375b416ab4c
ann <luzha_08@mail.ru>
2022-10-16 16:39:53 +0500

    initial commit

---
+++ Задача 25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную
+// степень B. Реализовать функцию возведения в степень
+// самостоятельно!
+
+Double Degree(int numA, int numB)
+{
+    double result = 1;
+
+    for (int i = 0; i < numB; i++)
+    {
+        result = result * numA;
+    }
+
+    return result;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число А:");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine(Degree(numberA, numberB));
---
+++ задача 27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт 
+// сумму цифр в числе.
+
+int getSum(int num)
+{
+    int result = 0;
+    
+    while (num > 0)
+    {
+        result = result + num % 10;
+        num = num / 10;        
+    }
+
+    return result;
+}
+Console.Clear();
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(getSum(number));
